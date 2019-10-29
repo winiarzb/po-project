4e7d9b78bb66f73ce9078f52470ccbacb4947b42
Seweryn Gładysz <seweryngla@hotmail.com>
2019-10-29 18:51:18 +0100

    contracts components

---
+++ src/web/bounty-hunter-web/src/app/modules/contract/components/contracts-list/contracts-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bh-contracts-list',
+  templateUrl: './contracts-list.component.html',
+  styleUrls: ['./contracts-list.component.scss']
+})
+export class ContractsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
