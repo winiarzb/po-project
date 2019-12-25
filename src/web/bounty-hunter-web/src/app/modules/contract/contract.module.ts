e166da9124c800704cae84441fe3288ff1643122
winiarzb <l3arteq@gmail.com>
2019-12-25 13:59:11 +0100

    CreateContractForm

--- src/web/bounty-hunter-web/src/app/modules/contract/contract.module.ts
+++ src/web/bounty-hunter-web/src/app/modules/contract/contract.module.ts
@@ -4,13 +4,21 @@ import { CommonModule } from '@angular/common';
 import { ContractRoutingModule } from './contract-routing.module';
 import { ContractsListComponent } from './components/contracts-list/contracts-list.component';
 import { CreateContractComponent } from './components/create-contract/create-contract.component';
-
+import {SharedModule} from "../../shared/shared.module";
+import {ReactiveFormsModule} from "@angular/forms";
+import {DxFormModule} from 'devextreme-angular';
 
 @NgModule({
   declarations: [ContractsListComponent, CreateContractComponent],
   imports: [
     CommonModule,
-    ContractRoutingModule
+    ContractRoutingModule,
+    SharedModule,
+    ReactiveFormsModule,
+    DxFormModule
+  ],
+  exports: [
+    CreateContractComponent
   ]
 })
 export class ContractModule { }
