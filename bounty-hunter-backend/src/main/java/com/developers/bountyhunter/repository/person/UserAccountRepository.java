7b1af7d082fd5978c25dbff666e40201304cce4b
krzysiekaw18 <krzysztof.stuglik@interia.pl>
2019-12-27 16:27:14 +0100

    security

--- bounty-hunter-backend/src/main/java/com/developers/bountyhunter/repository/person/UserAccountRepository.java
+++ bounty-hunter-backend/src/main/java/com/developers/bountyhunter/repository/person/UserAccountRepository.java
@@ -4,6 +4,10 @@ import com.developers.bountyhunter.model.person.UserAccount;
 import com.developers.bountyhunter.repository.base.BaseRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserAccountRepository extends BaseRepository<UserAccount, Long> {
+
+    Optional<UserAccount> findByUsername(String username);
 }
