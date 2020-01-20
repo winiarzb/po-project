607ed6e1aaf850345ae06697dc393b8e26266eec
Adrian Michałek <devmichalek@gmail.com>
2020-01-20 21:28:45 +0100

    Add some changes

--- bounty-hunter-backend/src/main/java/com/developers/bountyhunter/service/contract/ContractService.java
+++ bounty-hunter-backend/src/main/java/com/developers/bountyhunter/service/contract/ContractService.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Service;
 
 @Service
 public interface ContractService extends BaseService<Contract, Long> {
+    boolean changeContractStatusWhenAuctionIsFinished();
 }
--- bounty-hunter-backend/src/main/java/com/developers/bountyhunter/service/contract/ContractServiceImpl.java
+++ bounty-hunter-backend/src/main/java/com/developers/bountyhunter/service/contract/ContractServiceImpl.java
@@ -1,10 +1,13 @@
 package com.developers.bountyhunter.service.contract;
 
 import com.developers.bountyhunter.model.contract.Contract;
+import com.developers.bountyhunter.model.contract.ContractStatus;
 import com.developers.bountyhunter.repository.contract.ContractRepository;
 import com.developers.bountyhunter.service.base.BaseServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -12,4 +15,15 @@ public class ContractServiceImpl extends BaseServiceImpl<Contract, Long> impleme
 
 	private final ContractRepository contractRepository;
 
+	@Override
+	public boolean changeContractStatusWhenAuctionIsFinished() {
+		List<Contract> contracts = contractRepository.contractAuctionOver();
+		contracts.forEach(contract -> {
+			contract.setContractStatus(ContractStatus.CANCELED);
+			contractRepository.save(contract);
+		});
+		return true;
+	}
 }
+
+
