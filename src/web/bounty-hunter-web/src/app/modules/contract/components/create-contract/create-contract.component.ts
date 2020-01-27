47e4eb6498f4a973bd10ac40448212144d707539
Bartek <Bartek>
2020-01-27 21:27:42 +0100

    added create contract service

--- src/web/bounty-hunter-web/src/app/modules/contract/components/create-contract/create-contract.component.ts
+++ src/web/bounty-hunter-web/src/app/modules/contract/components/create-contract/create-contract.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {ResourceCreateComponentBase} from '../../../resources/components/create-forms/resource-create-component.abstract';
+import {ContractCreateRequest} from '../../../resources/models/contract/contract-create-request.model';
+import {ResourceStateService} from '../../../resources/services/resource-state.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'bh-create-contract',
   templateUrl: './create-contract.component.html',
   styleUrls: ['./create-contract.component.scss']
 })
-export class CreateContractComponent implements OnInit {
+// export class CreateContractComponent implements OnInit {
+//
+//   constructor() { }
+//
+//   ngOnInit() {
+//   }
+//
+// }
+export class CreateContractComponent extends ResourceCreateComponentBase<ContractCreateRequest> {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    @Inject('ResContractStateService') stateService: ResourceStateService<ContractCreateRequest>,
+    activatedRoute: ActivatedRoute
+  ) {
+    super(stateService, activatedRoute);
   }
-
 }
+
--- src/web/bounty-hunter-web/src/app/modules/contract/contract.module.ts
+++ src/web/bounty-hunter-web/src/app/modules/contract/contract.module.ts
@@ -8,6 +8,21 @@ import {SharedModule} from "../../shared/shared.module";
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {DxButtonModule, DxDataGridModule, DxFormModule} from 'devextreme-angular';
 import { ContractAuctionComponent } from './components/contract-auction/contract-auction.component';
+import {ResDictionaryRequestFactoryService} from '../resources/services/request-services/res-dictionary-request-factory.service';
+import {ResDistrictRequestFactoryService} from '../resources/services/request-services/res-district-request-factory.service';
+import {ResGalaxyRequestFactoryService} from '../resources/services/request-services/res-galaxy-request-factory.service';
+import {ResPlanetRequestFactoryService} from '../resources/services/request-services/res-planet-request-factory.service';
+import {ResReviewRequestFactoryService} from '../resources/services/request-services/res-review-request-factory.service';
+import {ResUserAccountRequestFactoryService} from '../resources/services/request-services/res-user-account-request-factory.service';
+import {ResVictimRequestFactoryService} from '../resources/services/request-services/res-victim-request-factory.service';
+import {ResContractRequestFactoryService} from '../resources/services/request-services/res-contract-request-factory.service';
+import {ResourcesApiService} from '../../shared/services/resources-api.service';
+import {RequestFactoryServiceBase} from '../../shared/services/request-factory-service.abstract';
+import {ResourceStateService} from '../resources/services/resource-state.service';
+
+const resourceStateServiceFactoryProvider = <TModel>(apiService: ResourcesApiService<TModel>, requestFactory: RequestFactoryServiceBase) => {
+  return new ResourceStateService<TModel>(apiService, requestFactory);
+};
 
 @NgModule({
   declarations: [ContractsListComponent, CreateContractComponent, ContractAuctionComponent],
@@ -23,6 +38,13 @@ import { ContractAuctionComponent } from './components/contract-auction/contract
   ],
   exports: [
     CreateContractComponent
+  ],
+  providers: [
+    {
+      provide: 'ResContractStateService',
+      useFactory: resourceStateServiceFactoryProvider,
+      deps: ['ContractApiService', ResContractRequestFactoryService]
+    },
   ]
 })
 export class ContractModule { }
