9b85449252d6ef33189215f4e00732fa3d4ea78c
Bartek <Bartek>
2020-01-28 20:43:04 +0100

    deleted unnecessary imports

--- src/web/bounty-hunter-web/src/app/modules/contract/contract.module.ts
+++ src/web/bounty-hunter-web/src/app/modules/contract/contract.module.ts
@@ -8,17 +8,6 @@ import {SharedModule} from '../../shared/shared.module';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {DxButtonModule, DxDataGridModule, DxFormModule} from 'devextreme-angular';
 import { ContractAuctionComponent } from './components/contract-auction/contract-auction.component';
-import {ResDictionaryRequestFactoryService} from '../resources/services/request-services/res-dictionary-request-factory.service';
-import {ResDistrictRequestFactoryService} from '../resources/services/request-services/res-district-request-factory.service';
-import {ResGalaxyRequestFactoryService} from '../resources/services/request-services/res-galaxy-request-factory.service';
-import {ResPlanetRequestFactoryService} from '../resources/services/request-services/res-planet-request-factory.service';
-import {ResReviewRequestFactoryService} from '../resources/services/request-services/res-review-request-factory.service';
-import {ResUserAccountRequestFactoryService} from '../resources/services/request-services/res-user-account-request-factory.service';
-import {ResVictimRequestFactoryService} from '../resources/services/request-services/res-victim-request-factory.service';
-import {ResContractRequestFactoryService} from '../resources/services/request-services/res-contract-request-factory.service';
-import {ResourcesApiService} from '../../shared/services/resources-api.service';
-import {RequestFactoryServiceBase} from '../../shared/services/request-factory-service.abstract';
-import {ResourceStateService} from '../resources/services/resource-state.service';
 
 @NgModule({
   declarations: [ContractsListComponent, CreateContractComponent, ContractAuctionComponent],
